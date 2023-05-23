ecfd95ec0112df254b150d1f6218779cb17fe124
Deepak kadela <deepakkadela21@gmail.com>
2023-05-23 17:46:36 +0530

    Updated Project

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -7,32 +7,30 @@ export const shazamCoreApi = createApi({
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
-        import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+        "9bcd970079msh106842d003579cap18dcd4jsn0d00251d0cf5"
       );
 
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
+    getTopCharts: builder.query({ query: () => "/v1/charts/world" }),
     getSongsByGenre: builder.query({
-      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+      query: (genre) => `/v1/charts/genre-world?genre_code=${genre}`,
     }),
     getSongsByCountry: builder.query({
-      query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
+      query: (countryCode) => `/v1/charts/country?country_code=${countryCode}`,
     }),
     getSongsBySearch: builder.query({
       query: (searchTerm) =>
-        `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+        `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
     }),
-    getArtistDetails: builder.query({
-      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
-    }),
+
     getSongDetails: builder.query({
-      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+      query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`,
     }),
     getSongRelated: builder.query({
-      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
+      query: ({ songid }) => `/v1/tracks/related?track_id=${songid}`,
     }),
   }),
 });
